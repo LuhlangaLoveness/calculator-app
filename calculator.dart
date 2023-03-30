e67dcfd7aa04c51a81256440aa2301f0330827b1
Luhlanga Loveness <luhlanga.loveness@gmail.com>
2023-03-30 15:40:50 +0200

    water

---
+++ calculator.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+import 'dart:math';
+
+double numInput() {
+  print("Enter the number for operation: ");
+  double myNumber = double.parse(stdin.readLineSync()!);
+  return myNumber;
+}
+
+String operandChoice() {
+  print("Enter the operand e.g '+', '-', '*', '/'");
+  String operandInput = stdin.readLineSync()!;
+  return operandInput;
+}
+
+void main(List<String> args) {
+  double num1 = numInput();
+  double num2 = numInput();
+  String choice = operandChoice();
+  double answer = 0.0;
+
+  if (choice == '+') {
+    answer = num1 + num2;
+    print("${num1} ${choice} {$num2} = ${answer}");
+  } else if (choice == '-') {
+    answer = num1 - num2;
+    print("${num1} ${choice} {$num2} = ${answer}");
+  } else {
+    print('Invalid input, please enter review');
+  }
+  print("${num1} ${choice} {$num2} = ${answer}");
+}
